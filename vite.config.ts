649b3075bca2cc18d9b220c946db2467fe2a3a19
patriciasmashp <patriciasmashlol@mail.ru>
2025-05-02 11:21:09 +0500

    build path

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build:{
-    outDir: process.env.NODE_ENV === 'production' ? '/var/www/' : './public',
-  },
   plugins: [
     vue(),
     vueDevTools(),
